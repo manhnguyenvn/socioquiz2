7954a24644193452526331cbf29234f82b4cd10d
manhnguyenvn <manhnguyenvn@yahoo.com>
2018-01-22 22:26:54 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php 
+ return array (
+  'DB_CONNECTION' => 'mysql',
+  'DB_HOST' => 'localhost',
+  'DB_DATABASE' => 'socioquiz',
+  'DB_USERNAME' => 'socioquiz',
+  'DB_PASSWORD' => 'serect',
+  'ADMIN_USERNAME' => 'admin',
+  'ADMIN_PASSWORD' => 'admin',
+);
